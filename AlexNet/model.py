9c2896a017f265e7b0edc638e71846ce12bda775
like-a-robot <911762858@qq.com>
2025-11-10 22:59:14 +0800

    AlexNet

--- AlexNet/model.py
+++ AlexNet/model.py
@@ -1,8 +1,43 @@
 import torch
 from torch import nn
+from torch.nn import Dropout
 from torchsummary import summary
 
 class AlexNet(nn.Module):
     def __init__(self):
         super(AlexNet, self).__init__()
-        
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=96,kernel_size=11,stride=4,padding=2)
+        self.pool = nn.MaxPool2d(kernel_size=3,stride = 2)
+        self.conv2 = nn.Conv2d(in_channels=96,out_channels=256,kernel_size=5,padding=2)
+        self.conv3 = nn.Conv2d(in_channels=256,out_channels=384,kernel_size=3,padding=1)
+        self.conv4 = nn.Conv2d(in_channels=384,out_channels=384,kernel_size=3,padding=1)
+        self.conv5 = nn.Conv2d(in_channels=384,out_channels=256,kernel_size=3,padding=1)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(in_features=256 * 6 * 6,out_features=4096)
+        self.fc2 = nn.Linear(in_features=4096,out_features=4096)
+        self.fc3 = nn.Linear(in_features=4096,out_features=10)
+        self.Relu = nn.ReLU()
+        self.Dropout = nn.Dropout(p=0.5)
+
+
+    def forward(self,x):
+        x = self.Relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.Relu(self.conv2(x))
+        x = self.pool(x)
+        x = self.Relu(self.conv3(x))
+        x = self.Relu(self.conv4(x))
+        x = self.Relu(self.conv5(x))
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = self.Dropout(x)
+        x = self.Relu(self.fc1(x))
+        x = self.Dropout(x)
+        x = self.Relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = AlexNet().to(device)
+    print(summary(model,(1,224,224)))
---
+++ AlexNet/model_train.py
@@ -0,0 +1,112 @@
+import torch
+from torchvision import transforms
+from torchvision.datasets import FashionMNIST
+import torch.utils.data as Data
+from model import AlexNet
+import torch.nn as nn
+import torch.optim as optim
+import copy
+import time
+import pandas as pd
+
+def train_val_data_process():
+    train_data = FashionMNIST(root='./data',
+                              transform=transforms.Compose([transforms.Resize(size=224),
+                                                           transforms.ToTensor()]),
+                              train=True,
+                              download=True)
+    train_data,val_train = Data.random_split(train_data,[round(len(train_data)*0.8),round(len(train_data)*0.2)])
+    train_loader = Data.DataLoader(dataset=train_data,
+                                   batch_size=16,
+                                   shuffle=True,
+                                   num_workers=2)
+    val_loader = Data.DataLoader(dataset=val_train,
+                                   batch_size=16,
+                                   shuffle=False,
+                                   num_workers=2)
+    return train_loader,val_loader
+
+def train_model_process(model, train_loader, val_loader, epochs):
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    optimizer = optim.Adam(model.parameters(),lr = 0.001)
+    criterion = nn.CrossEntropyLoss()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    since = time.time()
+
+    train_accs = []
+    train_losses = []
+    val_accs = []
+    val_losses = []
+
+    for epoch in range(epochs):
+        print('Epoch {}/{}'.format(epoch+1, epochs))
+
+        train_loss = 0.0
+        train_acc = 0.0
+        train_num = 0
+
+        val_loss = 0.0
+        val_acc = 0.0
+        val_num = 0
+
+        for step,(inputs,labels) in enumerate(train_loader):
+            inputs, labels = inputs.to(device), labels.to(device)
+            model.train()
+            outputs = model(inputs)
+            pred = torch.argmax(outputs,dim=1)
+
+            loss = criterion(outputs,labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() * inputs.size(0)
+            train_acc += (pred == labels).sum().item()
+            train_num += inputs.size(0)
+
+        for step,(inputs,labels) in enumerate(val_loader):
+            inputs, labels = inputs.to(device), labels.to(device)
+            model.eval()
+            outputs = model(inputs)
+            pred = torch.argmax(outputs,dim=1)
+
+            val_loss += criterion(outputs,labels).item() * inputs.size(0)
+            val_acc += (pred == labels).sum().item()
+            val_num += inputs.size(0)
+
+        train_losses.append(train_loss / train_num)
+        train_accs.append(train_acc / train_num)
+        val_losses.append(val_loss / val_num)
+        val_accs.append(val_acc / val_num)
+
+        print("Train loss %f, Train acc %f" % (train_losses[-1], train_accs[-1]))
+        print("Val loss %f, Val acc %f" % (val_losses[-1], val_accs[-1]))
+
+        if val_accs[-1] > best_acc:
+            best_acc = val_accs[-1]
+            best_model_wts = copy.deepcopy(model.state_dict())
+        time_elapsed = time.time() - since
+        print("Training complete in {:.0f}m {:.0f}s\n".format(time_elapsed // 60, time_elapsed % 60))
+
+    torch.save(best_model_wts,'./best_model_wts.pth')
+    train_process = pd.DataFrame(data={
+        "epoch":range(1,epochs+1),
+        'train_losses': train_losses,
+        'train_accs': train_accs,
+        'val_losses': val_losses,
+        'val_accs': val_accs,
+    })
+
+    return train_process
+
+if __name__ == '__main__':
+    model = AlexNet()
+    train_loader,val_loader = train_val_data_process()
+    # print(len(train_loader),len(val_loader))
+    train_process = train_model_process(model,train_loader,val_loader,epochs=10)
+    print(train_process)
+
